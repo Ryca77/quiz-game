705c0440b07294e53140c11f04de84da767b41d3
Ryca77 <ryan.carter77@gmail.com>
2016-05-24 16:32:30 +0100

    css/syle.css

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
 
+//Select Answer//
 $('.pic4').click(function() {
 	$('.pic1, .pic2, .pic3').css({"webkit-filter" : "opacity(0.5)"})
 });
 
+//Display Answer Modal//
+$('.submit').click(function() {
+	$('.answer-overlay').fadeIn(1000);
+});
+
+//Hide Answer Model & Launch Next Question//
+$('.next-question').click(function() {
+	$('.answer-overlay').fadeOut(1000);
+});
 
 });
