e27875d7b9142d8a4bed98985d2007b23b7cbe58
Ryca77 <ryan.carter77@gmail.com>
2016-05-27 11:26:41 +0100

    js/app.js

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
 $(document).ready(function(){
 
-//questions & answers array//
+//questions and answers array//
 var i = 0;
 var allQuestions = [ {
 	questionNumber: 1,
 	question: 'Who is the most successful manager of Manchester United?',
 	options: ['Sir Matt Busby', 'Louis Van Gaal', 'Ryan Giggs', 'Sir Alex Ferguson'],
-	answer: 3,
+	answer: 'Sir Alex Ferguson',
 	info: 'Sir Alex Ferguson was the manager of Manchester United for 26 years between 1986 and 2013. During that time he amassed 38 trophies including 13 Premier League titles, five FA Cups and two UEFA Champions League titles. His win rate was nearly 60 per cent, the highest of any manager in the history of the club.'
 	},
 			 {
 	questionNumber: 2,
 	question: 'Where do Manchester United play their home games?',		 	
 	options: ['Old Trafford', 'Emirates', 'Stamford Bridge', 'Anfield'],
-	answer: 0,
+	answer: 'Old Trafford',
 	info: 'Manchester United moved to Old Trafford in 1910 and have played there ever since (although from 1941 to 1949 the club shared with local rivals Manchester City as a result of Second World War bomb damage) With a capacity of 75,635 it is the largest club stadium of any club in the UK.'
 	}, 
 			 {
 	questionNumber: 3,
 	question: 'Who is the all time top goal scorer at Machester United?',
 	options: ['Cristiano Ronaldo', 'Sir Bobby Charlton', 'Wayne Rooney', 'George Best'],
-	answer: 1,
+	answer: 'Sir Bobby Charlton',
 	info: 'Sir Bobby Charlton has held the top goalscorer record since 1973. He scored a total of 249 goals over a 17 year period. Wayne Rooney is currently on 245 goals and is likely to surpass Sir Bobby during the 2016/17 season.'
 	},
 			 {
 	questionNumber: 4,
 	question: 'What was the original name of Manchester United when it was first formed in 1878?',
 	options: ['Newton Heath LYR F.C.', 'Salford City', 'FC United of Manchester', 'Irlam F.C.'],
-	answer: 0,
+	answer: 'Newton Heath LYR F.C.',
 	info: 'Newton Heath LYR Football Club was formed in 1878 by the Carriage and Wagon department of the Lancashire and Yorkshire Railway depot at Newton Heath. The club was rescued from financial problems in 1902 by four local businessmen, and the club renamed Manchester United Football Club by the new owners.'
 	},
 			 {
 	questionNumber: 5,
 	question: 'Who has made the most appearances for Manchester United?',
 	options: ['Paul Scholes', 'Mark Hughes', 'Ryan Giggs', 'David Beckham'],
-	answer: 2,
+	answer: 'Ryan Giigs',
 	info: 'Ryan Giggs made 963 appearances for Manchester United over a 23 year career at the club. This is over 200 more than any other player and it is thought that this record will never be beaten.'
 	},
 ];
@@ -90,7 +90,6 @@ $('.submit').hide();
 $('#go').on('click', function() {
 	$('.start').hide();
 	generateQuestions();
-
 });
 
 function generateQuestions() {
@@ -111,31 +110,41 @@ function generateQuestions() {
     console.log(firstOptions);
 };
 
-//select and check answer//
-function checkAnswer(user, answer) {
-	var userChoice = $('li').on('click', function() {
-		console.log(userChoice);
+//select option and check with answer//
+function checkAnswer() {
+	$('li').click(function() {
+	console.log($(this).text());
+	var userChoice = $(this).text();
+	var currentAnswer = allQuestions[i].answer;
+	var result = "";
 	if (userChoice === currentAnswer) {
-		return "Correct!"
+		console.log('correct');
+		result += "Correct!";
 	}
-	else if (userChoice !== currentAnswer) {
-		return "Incorrect!"
+	else {
+		console.log('incorrect');
+		result += "Incorrect!";
 	}
-	$('.answer').html(checkAnswer(userChoice, currentAnswer));
-	console.log(checkAnswer);
-});
+	return result;
+	});
 };
+checkAnswer();
+
+//didn't work but keeping for reference//
+/*sessionStorage.setItem('livalue', $(this).text());
+var userChoice = sessionStorage.getItem('livalue');*/
 
 //submit answer and show answer modal//
 $('.submit').on('click', function() {
 	$('.answer-overlay').show();
 	submitAnswer();
+	sessionStorage.clear();
 });
 
 function submitAnswer() {
 	$('.answer-overlay').show();
 	var currentAnswer = allQuestions[i].answer;
-		$('.answer').html(currentAnswer);
+		$('.answer').html("The answer is" + " " + currentAnswer);
 		console.log(currentAnswer);
 	var currentInfo = allQuestions[i].info;
 		$('.answer-info').html(currentInfo);
